0a502f0a2017ede6876a4aa66011542b3ad4ac98
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-02-12 23:18:47 +0200

    get all data from db

--- api/data/db.js
+++ api/data/db.js
@@ -1,17 +1,52 @@
-var config = require('./config')
+var firebase = require('firebase');
+var serviceAccount = require("./../../fireBaseKey/fairy-tales-kay.json");
 
-var firebase = require("firebase");
-
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./fireBaseKey/fairy-tales-kay.json");
-
-var APP = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fairy-tales-d7dde.firebaseio.com"
+firebase.initializeApp({
+  "serviceAccount": serviceAccount,
+  "databaseURL": "https://fairy-tales-d7dde.firebaseio.com"
 });
 
+var ref = firebase.app().database().ref();
+var usersRef = ref.child('users');
 
-// console.log(APP);
+var tailsRef = ref.child('talesList');
+console.log(tailsRef);
 
+tailsRef.orderByKey().on('child_added', function(snap) {
+        console.log(snap.getKey(), snap.val());
+      });
+
+
+// var delayedPush = function (user) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       usersRef.push(user)
+//           .then(resolve, reject);
+//     }, 1);
+//   });
+// };
+//
+// delayedPush({
+//   name: 'First User',
+//   time: (new Date()).getTime()
+// })
+//     .then(function() {
+//       return delayedPush({
+//         name: 'Second User',
+//         time: (new Date()).getTime()
+//       });
+//     })
+//     .then(function() {
+//       return delayedPush({
+//         name: 'Third User',
+//         time: (new Date()).getTime()
+//       });
+//     })
+//     .then(function() {
+//       usersRef.orderByKey().on('child_added', function(snap) {
+//         console.log(snap.getKey(), snap.val());
+//       });
+//     })
+//     .catch(function(err) {
+//       console.log('error', err);
+//     });
--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var bodyParser = require('body-parser')
 var path = require('path')
@@ -6,7 +5,43 @@ var app = express();
 
 var http = require('http');
 var https = require("https");
+var firebase = require("firebase");
+var admin = require("firebase-admin");
+
+const config = require('./config')
+// const todo = require('./todo/todo')
+//
+var db =  require('./api/data/db');
+
+app.listen(config.port, function () {
+  console.log(`Server running at port: ${config.port}`)
+});
+
+// app.use(bodyParser.json())
+//
+// app.use('/api/v1', todo)
+
+// error handling
+app.use(function (req, res, next) {
+  const err = new Error(`Not Found ${req.path}`)
+  err.status = 404
+  next(err)
+});
+app.use(function (error, req, res, next) {
+  if (error) {
+    console.log(error)
+    return res.status(400).json({error})
+  }
+  next(error)
+});
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+});
 
 
 
-
+module.exports = app;
