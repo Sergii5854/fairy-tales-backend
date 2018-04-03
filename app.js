f247e02b963f319171aa0654a28253d3cb234ea4
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-04-03 08:56:10 +0300

    add CORS

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ app.use(function(req, res, next) {
     next();
 });
 
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.use('/api/v1', usersRouter);
 app.use('/api/v1', fairytalesRouter);
 
