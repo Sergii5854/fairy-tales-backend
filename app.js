7b6007e568661e9358ab3719799cf81f8c56e19a
Vitalik7 <lavrenko.vitalik@yandex.ua>
2018-04-05 14:30:38 +0300

    fix app.js

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ app.use('/api/v1', router);
 app.use('/api/v1', user);
 app.use('/api/v1', admin);
 
-app.all('/*', handler);
-
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', function(req, res) {
     res.sendfile('./dist/index.html');
